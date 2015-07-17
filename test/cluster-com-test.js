218153979771d2806ba2e6eb0dbffda46e47cb59
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2015-07-17 15:04:39 -0600

    Updates.

--- test/cluster-com-test.js
+++ test/cluster-com-test.js
@@ -3,20 +3,9 @@
 let Commons = require('../lib/ut-commons');
 let Cluster = require('../lib/ut-cluster');
 
-let A = Cluster.system({
-  port: 2222,
-  logger: {
-    name: 'A'
-  }
-});
-let B = Cluster.system({
-  port: 2223,
-  logger: {
-    name: 'B'
-  },
-  peers: [ {
+let A = Cluster.server.bind({
+  server: {
     port: 2222,
     host: 'localhost'
-  }]
+  }
 });
-
